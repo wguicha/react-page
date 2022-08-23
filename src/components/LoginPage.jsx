a81bbb8fadf9f5440987bafb84bfc8f5e604e4e0
WilliamG <williamalberto1@hotmail.com>
2022-08-22 23:01:54 -0500

    Creada conexion con API

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,11 +1,13 @@
 import { useRef } from 'react';
 import { LockClosedIcon } from '@heroicons/react/solid';
+import { useRouter } from 'next/router';
 import { useAuth } from '@hooks/useAuth';
 
 export default function LoginPage() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const auth = useAuth();
+  const router = useRouter();
 
   const submitHanlder = (event) => {
     event.preventDefault();
@@ -13,7 +15,7 @@ export default function LoginPage() {
     const password = passwordRef.current.value;
 
     auth.signIn(email, password).then(() => {
-      console.log('Login success');
+      router.push('/dashboard');
     });
   };
 
